cf859d92c3da3c975255bbab994c7c942acfd40a
Simon-Mategen <62050394+Simon-Mategen@users.noreply.github.com>
2021-02-03 16:35:57 +0100

    done

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -3,27 +3,54 @@ package controller;
 import view.RecipeInserterGUI;
 import view.RelationsInserterGUI;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 
-public class Main {
-    public static void main(String[] args) throws SQLException {
+public class Main
+{
+    private void runInTerminal()
+    {
         Connector connector = new Connector();
-        //Un-comment to start the RecipeInserter
-        RecipeInserterGUI recGui = new RecipeInserterGUI(new RecipeInserter_SQL_Statements(connector));
 
-        //For starting the RealstionsInserter
-        //Has to be done in this order for it to work
-        RelationsInserter_SQL_Statements stmt = new RelationsInserter_SQL_Statements(connector);
-        RelationsInserterGUI relGui = new RelationsInserterGUI(stmt);
+        try
+        {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        //this is really weird, changed functions from private to public
-        RelationPriceInserter_SQL_Statements relPriceSql = new RelationPriceInserter_SQL_Statements(connector);
-        RelationPriceInserter relPrice = new RelationPriceInserter(relPriceSql);
-        relPrice.loadRelations();
-        relPrice.insertPrices();
-        // --------------------
+            System.out.println("\n" + "Choose program to run:" + "\n" +
+                    "1: RecipeInserter" + "\n" +
+                    "2: RelationsInserter" + "\n" +
+                    "3: PriceInserter (runs without GUI)");
 
-        stmt.setGui(relGui);
+            String s = reader.readLine();
 
+            switch (s)
+            {
+                case "1":
+                    RecipeInserterGUI recGui = new RecipeInserterGUI(new RecipeInserter_SQL_Statements(connector));
+                    break;
+                case "2":
+                    RelationsInserter_SQL_Statements stmt = new RelationsInserter_SQL_Statements(connector);
+                    RelationsInserterGUI relGui = new RelationsInserterGUI(stmt);
+                    stmt.setGui(relGui);
+                    break;
+                case "3":
+                    RelationPriceInserter_SQL_Statements relPriceSql = new RelationPriceInserter_SQL_Statements(connector);
+                    RelationPriceInserter relPrice = new RelationPriceInserter(relPriceSql);
+                    relPrice.loadRelations();
+                    relPrice.insertPrices();
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(" Main - Run in terminal: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws SQLException
+    {
+        Main prog = new Main();
+        prog.runInTerminal();
     }
 }
