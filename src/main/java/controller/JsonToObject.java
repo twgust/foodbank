a67f1b8cb195c58b1739f87eac1ad867fc53fce5
Fruited <56352131+Fruited@users.noreply.github.com>
2021-02-23 21:47:00 +0100

    Major rework on JsonToObject, restructuring, bug fixing and commenting.

--- src/main/java/controller/JsonToObject.java
+++ src/main/java/controller/JsonToObject.java
@@ -1,29 +1,36 @@
 package controller;
 
 import com.google.gson.*;
+import entity.Product;
 
 import java.io.FileReader;
 import java.nio.charset.StandardCharsets;
 import java.sql.Statement;
+import java.util.ArrayList;
+/*
+This class handles product data. It reads Json files to create a list of products which can be sent to the database.
+ */
 
 public class JsonToObject {
-    Connector connector = new Connector();
+    //Connector connector = new Connector();
 
-
-    //Product product = new Product();
-
-
-    private void parseJsonEx() {
+    /*
+    Takes a Json File of products and converts it into a list of Product objects.
+    Returns and ArrayList of Products.
+     */
+    //TODO: Should this be a static method?
+    private ArrayList<Product> parseJsonEx(String JsonFilename) {
+        ArrayList<Product> productArrayList = new ArrayList<>();
 
         try {
-            Statement st = connector.getConnection().createStatement();
-            // Product p = new Product();
+            //Statement st = connector.getConnection().createStatement();
             JsonParser parser = new JsonParser();
-            JsonArray a = (JsonArray) parser.parse(new FileReader("files/coopSort.json"));
+            JsonArray a = (JsonArray) parser.parse(new FileReader(JsonFilename, StandardCharsets.UTF_8));
 
             for (Object o : a) {
                 JsonObject p = (JsonObject) o;
 
+                //List of relevant product categories
                 String cat0 = "Dryck\n                    \n                        \n                            \n                                Pil";
                 String cat1 = "Vegetariskt\n                    \n                        \n                            \n                                Pil";
                 String cat2 = "Skafferi\n                    \n                        \n                            \n                                Pil";
@@ -37,70 +44,61 @@ public class JsonToObject {
                 String cat10 = "Frukt & Grönsaker\n                    \n                        \n                            \n                                Pil";
                 String cat11 = "Färdigmat & Mellanmål\n                    \n                        \n                            \n                                Pil";
 
-                //   String prod_name = String.valueOf(p.get("prod_name").getAsString());
-                // String prod_compPrice = String.valueOf(p.get("prod_compPrice").getAsString());
-                //  int idx = prod_price.lastIndexOf(':') +1;
-                //  String priceCorr = prod_price.substring(0, idx).replace(':', '.') + prod_price.substring(idx);
-
-
-                String categori = "";
+                String category = "";
                 String prod_name = "";
                 String prod_compPrice = "";
-                String prod_nname = "";
                 String unit = "";
+                String cleanPrice = "";
 
-                String priceCorr2 = "";
-                String priceCorr3 = "";
-                String priceCorr4 = "";
-                String priceCorr5 = "";
-                String priceCorr6 = "";
-                String priceCorr7 = "";
-                String priceCorr8 = "";
-                String priceCorr9 = "";
-
-                connector.getConnection();
-
+                //Gets the name of a product
                 if (p.get("prod_name") != null && !p.get("prod_name").isJsonNull()) {
                     prod_name = String.valueOf(p.get("prod_name").getAsString());
-                    prod_nname = prod_name.replace("'", "");
                 }
 
+                //Gets the price per unit of a product
                 if (p.get("prod_compPrice") != null && !p.get("prod_compPrice").isJsonNull()) {
                     prod_compPrice = String.valueOf(p.get("prod_compPrice").getAsString());
+
+                    //Finds the measure unit to the price
                     unit = detectUnit(prod_compPrice);
 
-                    priceCorr2 = prod_compPrice.replace("Jfr-pris ", "");
-                    priceCorr3 = priceCorr2.replace("/kr/kg.", "");
-                    priceCorr4 = priceCorr3.replace("/kr/lit.", "");
-                    priceCorr5 = priceCorr4.replace("/kr/st.", "");
-                    priceCorr6 = priceCorr5.replace("/utan sås/spad.", "");
-                    priceCorr7 = priceCorr6.replace("/kr/lit drickfärdig.", "");
-                    priceCorr8 = priceCorr7.replace(" ", "");
-                    priceCorr9 = priceCorr8.replace("kr/kg ätklar. ", "");
+                    //Isolates the floating point number from a longer string
+                    String price = prod_compPrice.replaceAll("[^0-9\\.]", "");
+                    StringBuilder builder = new StringBuilder();
+                    builder.append(price);
+                    builder.deleteCharAt(price.length()-1);
+                    cleanPrice = builder.toString();
                 }
 
+                //Gets the product category
                 if (p.get("Category") != null && !p.get("Category").isJsonNull()) {
-                    categori = String.valueOf(p.get("Category").getAsString());
+                    category = String.valueOf(p.get("Category").getAsString());
                 }
 
-                if ((categori.equals(cat0)) || (categori.equals(cat1)) || (categori.equals(cat2)) ||
-                        (categori.equals(cat3)) || (categori.equals(cat4)) || (categori.equals(cat5)) ||
-                        (categori.equals(cat6)) || (categori.equals(cat7)) || (categori.equals(cat8)) ||
-                        (categori.equals(cat9)) || (categori.equals(cat10)) || (categori.equals(cat11))) {
+                //Checks whether the product belongs to a relevant category
+                if ((category.equals(cat0)) || (category.equals(cat1)) || (category.equals(cat2)) ||
+                        (category.equals(cat3)) || (category.equals(cat4)) || (category.equals(cat5)) ||
+                        (category.equals(cat6)) || (category.equals(cat7)) || (category.equals(cat8)) ||
+                        (category.equals(cat9)) || (category.equals(cat10)) || (category.equals(cat11))) {
 
-                    String query = "Insert into FoodBankDB.dbo.Livsmedel(l_namn, l_pris, l_enhet) values('" + prod_name + "'," + priceCorr9 + ",'" + unit + "');";
-                    st.executeUpdate(query);
+                    productArrayList.add(new Product(prod_name, cleanPrice, unit));
+
+                    //String query = "Insert into FoodBankDB.dbo.Livsmedel(l_namn, l_pris, l_enhet) values('" + prod_name + "'," + priceCorr9 + ",'" + unit + "');";
+                    //st.executeUpdate(query);
                 }
             }
-            st.close();
-            connector.getConnection().close();
+            //st.close();
+            //connector.getConnection().close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
+        return productArrayList;
     }
 
+    /*
+    Takes in the string 'compPrice' and determines what type of unit per price it has.
+    Returns either st, lit or kg as unit.
+     */
     private String detectUnit(String compPrice) {
         String unit = "";
         if (compPrice.contains("kr/st")) {
@@ -112,21 +110,56 @@ public class JsonToObject {
         if (compPrice.contains("kr/lit")) {
             unit = "lit";
         }
-        if (compPrice.contains("utan sås/spad")){
+        if (compPrice.contains("utan")){
             unit = "kg";
         }
 
         return unit;
     }
 
+    /*
+    Generates an SQL query string for an insert operation of a product
+     */
+    public String generateQuery(Product product){
+
+            String name = product.getProd_name();
+            String price = product.getProd_price();
+            String unit = product.getUnit();
+            String query = "INSERT INTO FoodBankDB.dbo.Livsmedel(l_namn, l_pris, l_enhet) VALUES ('" + name + "'," + price + ",'" + unit + "');";
+
+            return query;
+    }
+
+    /*
+    Inserts every product in a list into the database
+     */
+    public void populateDB(ArrayList<Product> list){
+        for (int i = 0; i < list.size(); i++){
+            String query = generateQuery(list.get(i));
+            //execute statement with query above
+        }
+    }
+
 
     public static void main(String[] args) {
-
-
         JsonToObject o = new JsonToObject();
-        o.parseJsonEx();
+        ArrayList<Product> list = o.parseJsonEx("files/coopSort.json");
+        for(int i = 0; i < list.size(); i++){
+            String name = list.get(i).getProd_name();
+            String price = list.get(i).getProd_price();
+            String unit = list.get(i).getUnit();
+            System.out.println(String.format("%1$60s %2$8s %3$5s", name, price, unit));
+            //System.out.println(list.get(i).getProd_name() + ", " + list.get(i).getProd_price() + ", " + list.get(i).getUnit());
+        }
+        System.out.println(list.size());
 
-
+        /*String str = "Jfr-pris 95.63/utan sås/spad.";
+        String price = str.replaceAll("[^0-9\\.]", "");
+        StringBuilder builder = new StringBuilder();
+        builder.append(price);
+        builder.deleteCharAt(price.length()-1);
+        price = builder.toString();
+        System.out.println(price);*/
     }
 
 }
--- src/main/java/entity/Product.java
+++ src/main/java/entity/Product.java
@@ -1,53 +1,25 @@
 package entity;
 
-import java.util.ArrayList;
-
 public class Product {
-    public String category;
-    public String prod_name;
-    public String prod_price;
+    private String prod_name;
+    private String prod_price;
+    private String unit;
 
-    public Product(String prod_name, String prod_price, String category) {
-        this.category = category;
+    public Product(String prod_name, String prod_price, String unit) {
         this.prod_name = prod_name;
         this.prod_price = prod_price;
+        this.unit = unit;
     }
 
-
-
-
-    public String getCategory() {
-        return category;
-    }
-    public void setCategory(String category) {
-        this.category = category;
-    }
     public String getProd_name() {
         return prod_name;
     }
-    public void setProd_name(String prod_name) {
-        this.prod_name = prod_name;
-    }
+
     public String getProd_price() {
         return prod_price;
     }
-    public void setProd_price(String prod_price) {
-        this.prod_price = prod_price;
+
+    public String getUnit() {
+        return unit;
     }
-
-
-    /*
-    Lägger till Produktnamn , pris och kategori för varje produktobjekt i en arraylist.
-     */
-    public ArrayList<Product> addProducts(String prod_name, String prod_price, String category) {
-        ArrayList<Product> list = new ArrayList<Product>();
-        list.add(new Product(prod_name, prod_price, category));
-
-        return list;
-
-    }
-
-
-
-
 }
