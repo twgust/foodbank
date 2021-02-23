63470eb227daf03364493f803f9de0d3f29e6c83
Fruited <56352131+Fruited@users.noreply.github.com>
2021-02-23 12:46:36 +0100

    Units are now extracted from the Json file, and added along name and price into the database.

--- src/main/java/controller/JsonToObject.java
+++ src/main/java/controller/JsonToObject.java
@@ -3,24 +3,11 @@ package controller;
 import com.google.gson.*;
 
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
 
 public class JsonToObject {
     Connector connector = new Connector();
-
 
 
     //Product product = new Product();
@@ -50,18 +37,17 @@ public class JsonToObject {
                 String cat10 = "Frukt & Grönsaker\n                    \n                        \n                            \n                                Pil";
                 String cat11 = "Färdigmat & Mellanmål\n                    \n                        \n                            \n                                Pil";
 
-             //   String prod_name = String.valueOf(p.get("prod_name").getAsString());
-               // String prod_compPrice = String.valueOf(p.get("prod_compPrice").getAsString());
-              //  int idx = prod_price.lastIndexOf(':') +1;
-              //  String priceCorr = prod_price.substring(0, idx).replace(':', '.') + prod_price.substring(idx);
-
+                //   String prod_name = String.valueOf(p.get("prod_name").getAsString());
+                // String prod_compPrice = String.valueOf(p.get("prod_compPrice").getAsString());
+                //  int idx = prod_price.lastIndexOf(':') +1;
+                //  String priceCorr = prod_price.substring(0, idx).replace(':', '.') + prod_price.substring(idx);
 
 
                 String categori = "";
                 String prod_name = "";
                 String prod_compPrice = "";
                 String prod_nname = "";
-
+                String unit = "";
 
                 String priceCorr2 = "";
                 String priceCorr3 = "";
@@ -74,15 +60,15 @@ public class JsonToObject {
 
                 connector.getConnection();
 
-                if(p.get("prod_name") != null && !p.get("prod_name").isJsonNull()) {
+                if (p.get("prod_name") != null && !p.get("prod_name").isJsonNull()) {
                     prod_name = String.valueOf(p.get("prod_name").getAsString());
                     prod_nname = prod_name.replace("'", "");
-
-
                 }
 
-                if(p.get("prod_compPrice") != null &&!p.get("prod_compPrice").isJsonNull()) {
+                if (p.get("prod_compPrice") != null && !p.get("prod_compPrice").isJsonNull()) {
                     prod_compPrice = String.valueOf(p.get("prod_compPrice").getAsString());
+                    unit = detectUnit(prod_compPrice);
+
                     priceCorr2 = prod_compPrice.replace("Jfr-pris ", "");
                     priceCorr3 = priceCorr2.replace("/kr/kg.", "");
                     priceCorr4 = priceCorr3.replace("/kr/lit.", "");
@@ -91,51 +77,52 @@ public class JsonToObject {
                     priceCorr7 = priceCorr6.replace("/kr/lit drickfärdig.", "");
                     priceCorr8 = priceCorr7.replace(" ", "");
                     priceCorr9 = priceCorr8.replace("kr/kg ätklar. ", "");
-
-
                 }
 
-
-                if(p.get("Category") != null &&!p.get("Category").isJsonNull()) {
+                if (p.get("Category") != null && !p.get("Category").isJsonNull()) {
                     categori = String.valueOf(p.get("Category").getAsString());
-
                 }
-
 
                 if ((categori.equals(cat0)) || (categori.equals(cat1)) || (categori.equals(cat2)) ||
                         (categori.equals(cat3)) || (categori.equals(cat4)) || (categori.equals(cat5)) ||
-                        (categori.equals(cat6)) || (categori.equals(cat7)) || (categori.equals(cat8)) || (categori.equals(cat9)) || (categori.equals(cat10)) || (categori.equals(cat11)))
-                {
-
-
-
-                   String query = "Insert into FoodBankDB.dbo.Livsmedel(l_namn,l_pris) values('"+prod_name+"',"+priceCorr9+");";
-                   st.executeUpdate(query);
-
-
+                        (categori.equals(cat6)) || (categori.equals(cat7)) || (categori.equals(cat8)) || (categori.equals(cat9)) || (categori.equals(cat10)) || (categori.equals(cat11))) {
+                    String query = "Insert into FoodBankDB.dbo.Livsmedel(l_namn, l_pris, l_enhet) values('" + prod_name + "'," + priceCorr9 + ",'" + unit + "');";
+                    st.executeUpdate(query);
                 }
-
-
             }
             st.close();
             connector.getConnection().close();
         } catch (Exception e) {
-        e.printStackTrace();
-          }
+            e.printStackTrace();
+        }
 
 
     }
 
+    private String detectUnit(String compPrice) {
+        String unit = "";
+        if (compPrice.contains("kr/st.")) {
+            unit = "st";
+        }
+        if (compPrice.contains("kr/kg.")) {
+            unit = "kg";
+        }
+        if (compPrice.contains("kr/lit.")) {
+            unit = "lit";
+        }
 
-        public static void main (String[]args){
+        return unit;
+    }
+
+
+    public static void main(String[] args) {
 
 
         JsonToObject o = new JsonToObject();
         o.parseJsonEx();
 
 
-
-        }
-
     }
+
+}
 
