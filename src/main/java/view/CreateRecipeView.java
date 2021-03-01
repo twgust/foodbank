cda6698ce78549c0a4cf9b4f139497fd8d71534a
sonjaperic <62721545+sonjaperic@users.noreply.github.com>
2021-03-01 20:15:08 +0100

    GUI

--- src/main/java/view/CreateRecipeView.java
+++ src/main/java/view/CreateRecipeView.java
@@ -1,5 +1,6 @@
 package view;
 
+import com.sun.java.accessibility.util.GUIInitializedListener;
 import controller.Controller;
 
 import javax.swing.*;
@@ -50,13 +51,14 @@ public class CreateRecipeView extends JFrame implements ActionListener {
     JButton btnAddRecipe = new JButton("Lägg till recept");
 
 
+    //GUI
     DefaultListModel listSearchModel = new DefaultListModel();
+    DefaultListModel testListModel = new DefaultListModel();
     JScrollPane searchScrollPane = new JScrollPane(searchList);
-
 
     Controller controller = null;
 
-
+    //GUI
     public CreateRecipeView(Controller c) {
         controller = c;
 
@@ -68,6 +70,7 @@ public class CreateRecipeView extends JFrame implements ActionListener {
         setLayoutManager();
     }
 
+    //GUI
     public void setLayoutManager() {
         con.setLayout(null);
         setLayout();
@@ -77,7 +80,7 @@ public class CreateRecipeView extends JFrame implements ActionListener {
     }
 
     /*
-    Placerar rutans komponenter på given plats med koordinater och storlekar.
+    Placerar rutans komponenter på given plats med koordinater(X,Y) och storlekar(W,H).
      */
     private void setLayout() {
 
@@ -85,13 +88,13 @@ public class CreateRecipeView extends JFrame implements ActionListener {
         lblRecipe.setBounds(60, 85, 200, 30);
         tfRecipename.setBounds(60, 110, 200, 30);
         lblPortion.setBounds(60, 135, 200, 30);
-        tfPortion.setBounds(60,160, 200, 30);
+        tfPortion.setBounds(60, 160, 200, 30);
         lblInstruction.setBounds(60, 190, 200, 30);
         instructionsArea.setBounds(60, 220, 200, 300);
 
         //Lägg till ingrediens i DB
         lblAddIng.setBounds(60, 550, 200, 35);
-        tfAddIng.setBounds(60,580, 200, 35);
+        tfAddIng.setBounds(60, 580, 200, 35);
         lblPris.setBounds(60, 620, 200, 35);
         tfPris.setBounds(60, 650, 200, 35);
         lblEnhet.setBounds(280, 550, 200, 35);
@@ -120,7 +123,7 @@ public class CreateRecipeView extends JFrame implements ActionListener {
 
 
     /*
-    Lägger till alla komponenter i Jframen. Uppdelat för varje "Sektion" Vänster -> höger.
+    Lägger till alla komponenter i Jframen.
      */
     public void addComponents() {
 
@@ -158,29 +161,39 @@ public class CreateRecipeView extends JFrame implements ActionListener {
 
 
         searchList.setModel(listSearchModel);
+        listIng.setModel(testListModel);
+
     }
 
 
     public String getSearchRep() {
         return tfSearchRec.getText();
     }
+
     public DefaultListModel getListIngModel() {
         return listSearchModel;
     }
+
+    public DefaultListModel getIngredientListModel() {
+        return testListModel;
+    }
+
     public JList getList() {
         return searchList;
     }
 
-
-
+    //ActionListener för knapparna
     public void btnActions() {
         btnSearch.addActionListener(this);
         btnAddIng.addActionListener(this);
+        btnAddIngredientToRecipe.addActionListener(this);
         btnAddRecipe.addActionListener(this);
     }
 
 
+    //Funktionalitet knappar
     @Override
+    //knapp för att söka i DB efter sökruta
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == btnSearch) {
             controller.getSearch();
@@ -198,12 +211,42 @@ public class CreateRecipeView extends JFrame implements ActionListener {
             }
         }
 
-        if (e.getSource() == btnAddRecipe) {
+        //knapp som lägger till ingrediens i receptet i GUI
+        if (e.getSource() == btnAddIngredientToRecipe) {
 
-            
+            String ingredient = searchList.getSelectedValue();      //hämtar markerad ingrediens
+
+            String amount = tfAmt.getText();                        //hämtar mängd av markerad ingrediens
+            int amount2 = Integer.parseInt(amount);
+
+            String total = amount2 + " " + ingredient ;             //sparar ingrediens + mängd
+
+            testListModel.addElement(total);                        //lägger till på ny rad i innehåll
+
+            tfAmt.setText("");                                      //rensar textfield mängd
+
+            }
+
+        //knapp som lägger till ett recept i DB via GUI
+            if (e.getSource() == btnAddRecipe) {
+
+                String recipeName = tfRecipename.getText();     //hämtar receptnamn
+
+                String portions = tfPortion.getText();          //hämtar antal portioner
+                int portions2 = Integer.parseInt(portions);
+
+                String description = instructionsArea.getText();  //hämtar instruktioner
+
+                //Test
+                System.out.println(recipeName);
+                System.out.println(portions2);
+                System.out.println(description);
+                System.out.println(testListModel);          //hämtar JList med receptingredienser
+
+            }
+
+
         }
-
 
     }
 
-}
