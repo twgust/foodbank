a92db819cce81086f1a6a23b994c0c0712d8e928
Fruited <56352131+Fruited@users.noreply.github.com>
2021-03-24 14:29:21 +0100

    Main class is now functional

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,53 +1,7 @@
 package controller;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.sql.SQLException;
-
-public class Main
-{
-   /* private void runInTerminal()
-    {
-        Connector connector = new Connector();
-
-        try
-        {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-            System.out.println("\n" + "Choose program to run:" + "\n" +
-                    "1: RecipeInserter" + "\n" +
-                    "2: RelationsInserter" + "\n" +
-                    "3: PriceInserter (runs without GUI)");
-
-            String s = reader.readLine();
-
-            switch (s)
-            {
-                case "1":
-                    RecipeInserterGUI recGui = new RecipeInserterGUI(new RecipeInserter_SQL_Statements(connector));
-                    break;
-                case "2":
-                    RelationsInserter_SQL_Statements stmt = new RelationsInserter_SQL_Statements(connector);
-                    RelationsInserterGUI relGui = new RelationsInserterGUI(stmt);
-                    stmt.setGui(relGui);
-                    break;
-                case "3":
-                    RelationPriceInserter_SQL_Statements relPriceSql = new RelationPriceInserter_SQL_Statements(connector);
-                    RelationPriceInserter relPrice = new RelationPriceInserter(relPriceSql);
-                    relPrice.loadRelations();
-                    relPrice.insertPrices();
-                    break;
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println(" Main - Run in terminal: " + e.getMessage());
-        }
+public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
     }
-
-    public static void main(String[] args) throws SQLException
-    {
-        Main prog = new Main();
-        prog.runInTerminal();
-    }*/
 }
